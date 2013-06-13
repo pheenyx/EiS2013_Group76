05226fc6cd809dece1813079727e398d66dddc97
Michael Mardaus <mardaus@students.uni-mainz.de>
2013-06-13 21:21:13 +0200

    completed aufgabe 2

--- Blatt8/src/Lists.java
+++ Blatt8/src/Lists.java
@@ -10,6 +10,7 @@ import java.util.LinkedList;
  */
 public class Lists {
     final static int times = 100000;
+    
     public static void main(String[] strings){
         ArrayList<Integer> arraylist = new ArrayList<Integer>();
         LinkedList<Integer> list = new LinkedList<Integer>();
@@ -19,6 +20,7 @@ public class Lists {
         long start, end;
         
         //insert first 1000000 numbers
+        System.out.println("Insertion of first bunch of numbers");
         start = System.currentTimeMillis();
         for (int i = 0; i < times; ++i){
 //            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
@@ -51,7 +53,154 @@ public class Lists {
         end = System.currentTimeMillis();
         System.out.println("smallarray " + (end-start));
         
+        //end of insertion 1
         
+        //insert "times" more numbers at "times/4"
+        System.out.println("insertion of second bunch of numbers");
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            arraylist.add(times/4, i);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("arraylist " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            list.add(times/4, i);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("list " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            bigarray[times/4] = i;
+        }
+        end = System.currentTimeMillis();
+        System.out.println("bigarray " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            smallarray = add(smallarray, i, times/4);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("smallarray " + (end-start));
+        
+        //end of second insertion
+        
+        //removing numbers
+        System.out.println("Removing first bunch of numbers");
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            arraylist.remove(times/2);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("arraylist " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            list.remove(times/2);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("list " + (end-start));
+        
+        start = System.currentTimeMillis();
+        int j = 0;
+        for (j = 0; j < times; ++j){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            bigarray[times/2 + j] = times + times/2 + j;
+        }
+        for (; j<bigarray.length;++j){
+        	bigarray[j] = 0;
+        }
+        end = System.currentTimeMillis();
+        System.out.println("bigarray " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            smallarray = remove(smallarray, times/2);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("smallarray " + (end-start));
+        
+        //end of removing
+        
+        //read the rest
+        System.out.println("reading the numbers");
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            arraylist.get(i);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("arraylist " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            list.get(i);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("list " + (end-start));
+        
+        start = System.currentTimeMillis();
+        int readVariable;
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            readVariable=bigarray[i];
+        }
+        end = System.currentTimeMillis();
+        System.out.println("bigarray " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            readVariable = smallarray[i];
+        }
+        end = System.currentTimeMillis();
+        System.out.println("smallarray " + (end-start));
+        
+        //end of reading
+        
+        //setting some values
+        System.out.println("setting some new values in");
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            arraylist.set(i, i);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("arraylist " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            list.set(i, i);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("list " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            bigarray[i] = i;
+        }
+        end = System.currentTimeMillis();
+        System.out.println("bigarray " + (end-start));
+        
+        start = System.currentTimeMillis();
+        for (int i = 0; i < times; ++i){
+//            if (i%10000==0) System.out.println(i+ " " + (System.currentTimeMillis()-start));
+            smallarray[i] = i;
+        }
+        end = System.currentTimeMillis();
+        System.out.println("smallarray " + (end-start));
         
 //        smallarray = add(smallarray, 23, 0);
 //        for (int i = 0; i < smallarray.length; ++i){
